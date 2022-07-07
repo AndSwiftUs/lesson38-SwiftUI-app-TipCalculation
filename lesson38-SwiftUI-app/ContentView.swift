3ffd268d5bbe7aa3f2c12117fb7e5c4d6505fdfb
Andrew Ustintchik <AndSwiftUs@gmail.com>
2022-07-07 20:58:50 +0300

    Initial Commit

---
+++ lesson38-SwiftUI-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lesson38-SwiftUI-app
+//
+//  Created by Andrew Ustintchik on 07.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lesson38-SwiftUI-app/lesson38_SwiftUI_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  lesson38_SwiftUI_appApp.swift
+//  lesson38-SwiftUI-app
+//
+//  Created by Andrew Ustintchik on 07.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct lesson38_SwiftUI_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
